310f061c418886691c228f0c3abf9a0a7d4228a5
iampkuhz <iampkuhz@gmail.com>
2025-01-20 23:06:28 +0800

    markdown preview 调试完成

--- bin/brew.sh
+++ bin/brew.sh
@@ -1,4 +1,4 @@
-
+brew install yarn
 
 # 代码formatter工具，neovim的formatter插件（conform.nvim等）会调用
 brew install shfmt stylua prettier fsouza/prettierd/prettierd
---
+++ neovim/lua/plugins/plugin-markdown-preview.lua
@@ -0,0 +1,11 @@
+-- 需要提前安装好yarn npm
+-- install with yarn or npm
+return {
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "cd app && yarn install",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+  end,
+  ft = { "markdown" },
+}
